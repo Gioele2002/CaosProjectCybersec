0696049a5e549b5f54e003032a450f6a37cca826
Gioele2002 <70599563+Gioele2002@users.noreply.github.com>
2024-12-02 15:01:26 +0100

    Add files via upload

--- hw/arm/s32k3x8evb.c
+++ hw/arm/s32k3x8evb.c
@@ -18,10 +18,11 @@
 #include "qapi/qmp/qlist.h"
 #include "qom/object.h"
 #include "hw/qdev-properties.h"
+#include "qemu/units.h"
 
 /* Base addresses and sizes */
-#define FLASH_BASE    0x00400000
-#define FLASH_SIZE    0x00800000  // 8 MB
+#define FLASH_BASE    0x00000000
+#define FLASH_SIZE    0x00800000 // 8 MB
 
 #define SRAM0_BASE    0x20400000
 #define SRAM0_SIZE    0x00040000  // 256 KB
@@ -78,34 +79,13 @@ static void s32k3x8evb_init(MachineState *machine)
     Error *err = NULL;
     
     DeviceState *armv7m;
-
-     /* Initialize CPU (Cortex-M7) */
-     //This function initializes the CPU (Cortex-M7) as a child object of the board (s), which is represented by the structure S32K3X8EVBState.
-    object_initialize_child(OBJECT(s), "armv7m", &s->armv7m, TYPE_ARMV7M);
-    //This line casts the armv7m object (which was initialized as part of S32K3X8EVBState) to a DeviceState type, which is a common type for all devices in QEMU.
-    armv7m = DEVICE(&s->armv7m);
-    //This function connects the system clock (s->sysclk) to the CPU (armv7m), which allows the CPU to run at the specified frequency.
-    qdev_connect_clock_in(armv7m, "cpuclk", s->sysclk);
-    // This function sets a property of the CPU device (armv7m). Specifically, it sets the cpu-type property to the CPU type defined in the machine.
-    qdev_prop_set_string(armv7m, "cpu-type", machine->cpu_type);
-    //This function sets a property of the CPU to enable a feature (in this case, the ARM bit-banding feature).
-    qdev_prop_set_bit(armv7m, "enable-bitband", true);
-    //This sets the memory property of the CPU to point to the system memory, meaning that the CPU will use the system memory region to fetch instructions and data.
-    object_property_set_link(OBJECT(&s->armv7m), "memory",
-                             OBJECT(get_system_memory()), &err);
-    //This function finalizes the initialization of the CPU and ensures it is properly connected to the system bus so that it can interact with other devices.
-    sysbus_realize(SYS_BUS_DEVICE(&s->armv7m), &err);
-    /*This function assigns the parent bus for the device (armv7m) to the system bus. The parent bus connects the CPU to the rest of the system, allowing it to interact with other devices and memory regions.*/
-    qdev_set_parent_bus(DEVICE(&s->armv7m), sysbus_get_default(), &err);
-
-    /* Initialize system clock */
+    
+     /* Initialize system clock */
     s->sysclk = clock_new(OBJECT(machine), "SYSCLK");
     clock_set_hz(s->sysclk, SYSCLK_FRQ);
-
     /* Initialize Flash memory */
-    memory_region_init_ram(&s->flash, NULL, "s32k3x8evb.flash", FLASH_SIZE, &err);
+    memory_region_init_rom(&s->flash, NULL, "s32k3x8evb.flash", FLASH_SIZE, &err);
     memory_region_add_subregion(get_system_memory(), FLASH_BASE, &s->flash);
-
     /* Initialize ITCM */
     memory_region_init_ram(&s->itcm, NULL, "s32k3x8evb.itcm", ITCM_SIZE, &err);
     memory_region_add_subregion(get_system_memory(), ITCM_BASE, &s->itcm);
@@ -126,6 +106,25 @@ static void s32k3x8evb_init(MachineState *machine)
     memory_region_init_ram(&s->sram2, NULL, "s32k3x8evb.sram2", SRAM2_SIZE, &err);
     memory_region_add_subregion(get_system_memory(), SRAM2_BASE, &s->sram2);
 
+     /* Initialize CPU (Cortex-M7) */
+     //This function initializes the CPU (Cortex-M7) as a child object of the board (s), which is represented by the structure S32K3X8EVBState.
+    object_initialize_child(OBJECT(s), "armv7m", &s->armv7m, TYPE_ARMV7M);
+    //This line casts the armv7m object (which was initialized as part of S32K3X8EVBState) to a DeviceState type, which is a common type for all devices in QEMU.
+    armv7m = DEVICE(&s->armv7m);
+    //This function connects the system clock (s->sysclk) to the CPU (armv7m), which allows the CPU to run at the specified frequency.
+    qdev_connect_clock_in(armv7m, "cpuclk", s->sysclk);
+    // This function sets a property of the CPU device (armv7m). Specifically, it sets the cpu-type property to the CPU type defined in the machine.
+    qdev_prop_set_string(armv7m, "cpu-type", machine->cpu_type);
+    //This function sets a property of the CPU to enable a feature (in this case, the ARM bit-banding feature).
+    qdev_prop_set_bit(armv7m, "enable-bitband", true);
+    //This sets the memory property of the CPU to point to the system memory, meaning that the CPU will use the system memory region to fetch instructions and data.
+    object_property_set_link(OBJECT(&s->armv7m), "memory",
+                             OBJECT(get_system_memory()), &err);
+    //This function finalizes the initialization of the CPU and ensures it is properly connected to the system bus so that it can interact with other devices.
+    sysbus_realize(SYS_BUS_DEVICE(&s->armv7m), &err);
+    /*This function assigns the parent bus for the device (armv7m) to the system bus. The parent bus connects the CPU to the rest of the system, allowing it to interact with other devices and memory regions.*/
+    qdev_set_parent_bus(DEVICE(&s->armv7m), sysbus_get_default(), &err);
+
     /* Initialize UART */
    // s->uart = sysbus_create_simple("pl011", UART_BASE, NULL);
 
@@ -133,7 +132,7 @@ static void s32k3x8evb_init(MachineState *machine)
     //s->can = sysbus_create_simple("can_sja1000", CAN_BASE, NULL);
 
     /* Load firmware into Flash */
-    armv7m_load_kernel(ARM_CPU(armv7m), machine->kernel_filename, 0, FLASH_SIZE);
+    armv7m_load_kernel(ARM_CPU(first_cpu), machine->kernel_filename, 0, FLASH_SIZE);
 }
 
 
@@ -151,7 +150,7 @@ static void s32k3x8evb_class_init(ObjectClass *oc, void *data)
     mc->desc = "NXP S32K348 EVB Board";
     mc->init = s32k3x8evb_init;
     mc->max_cpus = 1;
-    mc->default_ram_size = SRAM0_SIZE + SRAM1_SIZE + SRAM2_SIZE;
+    mc->default_ram_size = 1152 * KiB;
     mc->default_cpu_type = ARM_CPU_TYPE_NAME("cortex-m7");
     mc->valid_cpu_types = valid_cpu_types;
     
